54cd84bc8e80dd3c0659ff876f1b93645ab4a8ce
Daniel Gonçalves <167038408+Daniseph@users.noreply.github.com>
2024-06-02 23:31:51 +0100

    Update and rename ConfiguracoesPerfilForm.cs to ConfiguracoesPerfil.cs

--- ConfiguracoesPerfil.cs
+++ ConfiguracoesPerfil.cs
@@ -1,17 +1,18 @@
 // Base da implementação do formulário para as Configurações do Perfil
 using System;
-using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace PlatGestMoo
 {
-    public partial class ConfiguracoesPerfilForm : Form
+    public partial class ConfiguracoesPerfil
     {
         private Controller controller;
 
-        public ConfiguracoesPerfilForm(Controller ctrl)
+        public ConfiguracoesPerfil(Controller ctrl)
         {
             InitializeComponent();
             controller = ctrl;
+            controller.ConfiguracoesPerfilAtualizadas += ConfirmacaoConfiguracoesPerfil;
         }
 
         private void btnSalvar_Click(object sender, EventArgs e)
@@ -22,6 +23,11 @@ namespace PlatGestMoo
             bool receberNotificacoes = chkReceberNotificacoes.Checked;
             
             controller.SalvarConfiguracoesPerfil(nome, email, senha, receberNotificacoes);
+        }
+
+        public void ConfirmacaoConfiguracoesPerfil(string mensagem)
+        {
+            MessageBox.Show(mensagem, "Configurações Salvas", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
